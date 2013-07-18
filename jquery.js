fea1fe7a7e374a4a7f46cdb7ed9eb24e68c5da43
Shannon Yeh <shannon.yeh@me.com>
2013-07-17 22:10:34 -0400

    37signal clone

--- jquery.js
+++ jquery.js
@@ -7,6 +7,22 @@ jQuery(document).ready(function() {
 
 		
 	});
+		$('.highrise').mouseenter(function () {
+		$('.highrise img').show();
+	});
+	$('.highrise').mouseleave(function (){
+		$('.highrise img').hide();
+
+		
+	});
+		$('.campfire').mouseenter(function () {
+		$('.campfire img').show();
+	});
+	$('.campfire').mouseleave(function (){
+		$('.campfire img').hide();
+
+		
+	});
 });
 
 
